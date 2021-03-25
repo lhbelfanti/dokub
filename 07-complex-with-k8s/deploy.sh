be127c053fe7a4059e5d4d5b86f0bfc26690b21c
Lucas <lhbelfanti@gmail.com>
2021-03-25 02:56:28 -0300

    Fixed deploy issue.

--- 07-complex-with-k8s/deploy.sh
+++ 07-complex-with-k8s/deploy.sh
@@ -1,6 +1,8 @@
-docker build -t lhbelfanti/multi-client:latest -t lhbelfanti/multi-client:$SHA -f ./07-complex-with-k8s/client/Dockerfile ./07-complex-with-k8s/client
-docker build -t lhbelfanti/multi-server:latest -t lhbelfanti/multi-server:$SHA -f ./07-complex-with-k8s/server/Dockerfile ./07-complex-with-k8s/server
-docker build -t lhbelfanti/multi-worker:latest -t lhbelfanti/multi-worker:$SHA -f ./07-complex-with-k8s/worker/Dockerfile ./07-complex-with-k8s/worker
+cd 07-complex-with-k8s/
+
+docker build -t lhbelfanti/multi-client:latest -t lhbelfanti/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lhbelfanti/multi-server:latest -t lhbelfanti/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lhbelfanti/multi-worker:latest -t lhbelfanti/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push lhbelfanti/multi-client:latest
 docker push lhbelfanti/multi-server:latest
@@ -10,7 +12,7 @@ docker push lhbelfanti/multi-client:$SHA
 docker push lhbelfanti/multi-server:$SHA
 docker push lhbelfanti/multi-worker:$SHA
 
-kubectl apply -f ./07-complex-with-k8s/k8s
+kubectl apply -f k8s
 
 kubectl set image deployments/client-deployments client=lhbelfanti/multi-client:$SHA
 kubectl set image deployments/server-deployments server=lhbelfanti/multi-server:$SHA
